907744f844cb63b798944ee1b619a1e9b737bf17
Mohamed Mahmoud <m.raslan97@gmail.com>
2023-06-07 17:19:51 +0300

    initial project commit

---
+++ android/app/src/main/kotlin/com/example/mutli_user_2d_car_racing_game_with_group_chat_using_flutter_and_firebase_7june/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.mutli_user_2d_car_racing_game_with_group_chat_using_flutter_and_firebase_7june
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
